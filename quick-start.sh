cefb5b07d93ebd7bc5cebda202ea88fd22636252
Jacob Ellehauge <jacell@erst.dk>
2022-09-07 15:15:47 +0200

    happy to code

---
+++ quick-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+# sudo apt-get install build-essential libreadline-dev zlib1g-dev flex bison
+# make PG_CONFIG=/path/to/postgres/bin/pg_config install
