d9e891b3437af4cfcf0e2566d61e2dde85b9e09c
Jacob Ellehauge <jacell@erst.dk>
2022-09-14 14:47:57 +0200

    adding some queries and setting path

--- quick-start.sh
+++ quick-start.sh
@@ -3,5 +3,9 @@
 python3 -m venv .venv
 source .venv/bin/activate
 
+RUN git clone https://github.com/apache/incubator-age age
+
 # sudo apt-get install build-essential libreadline-dev zlib1g-dev flex bison
 # make PG_CONFIG=/path/to/postgres/bin/pg_config install
+
+# select * from pg_available_extensions where installed_version is not null ;
