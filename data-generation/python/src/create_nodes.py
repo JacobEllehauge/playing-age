2f049b8405758000bd2dd997161ccecb0b512061
Jacob Ellehauge <jacell@erst.dk>
2022-09-21 15:36:22 +0200

    working relations needs some structure

--- data-generation/python/src/create_nodes.py
+++ data-generation/python/src/create_nodes.py
@@ -5,25 +5,43 @@ import psycopg2
 
 def create_person(n=1)->List:
     fake = Faker()
-    return [fake.name() for i in range(0,n)]
-
+    return [[fake.name(), fake.address()] for i in range(0,n)]
 
 def make_node(nodetype, properties: Optional[Dict])->None:
 
     with psycopg2.connect("host=localhost port=5499  password='LOL' dbname=graphs user=postgres") as conn:
         with conn.cursor() as curs:
-            for name in create_person(n=100):
+            for _list in create_person(n=100):
                 q = f"""
                 SET search_path = ag_catalog;
                 SELECT *
                 FROM cypher('starting_here', $$
-                    CREATE (a:Person {{name: "{name}", title: 'Developer'}})
+                    CREATE (a:Person {{name: "{_list[0]}", adresse: "{_list[1]}"}})
                     RETURN a
                 $$) as (a agtype);
+
+                SELECT *
+                FROM cypher('starting_here', $$
+                    CREATE (a:Adresse {{name: "{_list[1]}"}})
+                    RETURN a
+                $$) as (a agtype);
+
+                SELECT *
+                FROM cypher('starting_here', $$
+                    MATCH (a:Person), (b:Adresse)
+                    WHERE a.name = "{_list[0]}" AND b.name = "{_list[1]}"
+                    CREATE (a)-[e:HAS_ADRESSE]->(b)
+                    RETURN e
+                $$) as (e agtype);
+
                 """
                 curs.execute(q)
 
 
+
+
+
+
 if __name__=="__main__":
-    print(f'hey there {create_person()[0]} ')
+    print(f'hey there {create_person()[0][0]} from {create_person()[0][1]} ')
     make_node(nodetype='Person', properties=None)
