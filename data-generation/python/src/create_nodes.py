ae6ff103b8e377f875e1ed829d493da92ebb8943
Jacob Ellehauge <jacell@erst.dk>
2022-09-28 16:13:47 +0200

    add viewer

--- data-generation/python/src/create_nodes.py
+++ data-generation/python/src/create_nodes.py
@@ -1,13 +1,24 @@
 from faker import Faker
 from typing import Dict, List, Optional
 import psycopg2
+from time import time
 
 
 def create_person(n=1)->List:
     fake = Faker()
     return [[fake.name(), fake.address()] for i in range(0,n)]
 
-def make_node(nodetype, properties: Optional[Dict])->None:
+def timer(func):
+    def wrap_func(*args, **kwargs):
+        t1 = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f'Function {func.__name__!r} executed in {(t2-t1):.2f}seconds')
+        return result
+    return wrap_func
+
+@timer
+def make_node(nodetype: str, no_nodes: int, properties: Optional[Dict])->None:
 
     with psycopg2.connect("host=localhost port=5499  password='LOL' dbname=graphs user=postgres") as conn:
         with conn.cursor() as curs:
@@ -39,9 +50,10 @@ def make_node(nodetype, properties: Optional[Dict])->None:
 
 
 
-
-
-
 if __name__=="__main__":
+    no_nodes = 100000
+
+
     print(f'hey there {create_person()[0][0]} from {create_person()[0][1]} ')
-    make_node(nodetype='Person', properties=None)
+    make_node(nodetype='Person', no_nodes=no_nodes, properties=None)
+    print(f"Created { no_nodes} nodes!! GZ")
